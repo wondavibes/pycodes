f2bf967056d33b5c644a6d5c0b20459a1c4e0dc7
wondavibes <amosayemoro2001@gmail.com>
2025-08-02 23:25:52 +0100

    corrected test

--- test/test_sort.py
+++ test/test_sort.py
@@ -1,9 +1,9 @@
 from chall import sort
 
 def test_sort():
-    assert sort("aBc123") == "abcB123"
-    assert sort("zYx9876543210") == "xyzY9876543210"
+    assert sort("aBc123") == "acB132"
+    assert sort("zYx9876543210") == "xzY9753108642"
     assert sort("") == "Invalid input, please enter alphanumeric characters only."
     assert sort("!@#") == "Invalid input, please enter alphanumeric characters only."
-    assert sort("abcABC123") == "abcABC123"
+    assert sort("abcABC123") == "abcABC132"
     assert sort("AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz") == "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
